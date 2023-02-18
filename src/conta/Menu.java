912dcab6d2d6ad1b2dcc71720c838624469c7ac7
lialaurindo <liamaralaurindo@gmail.com>
2023-02-17 23:31:45 -0300

    Estrutura de Menu e Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,9 +1,81 @@
 package conta;
 
+import java.util.Scanner;
+
+import conta.model.Conta;
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		Conta c = new Conta(1, 123, 1, "Maria Silva", 10000.0f);
+		int opcao;
+		
+		c.visualizar();
+		System.out.println(c.getSaldo());
+		
+		c.depositar(1000);
+		c.sacar(500);
+		System.out.println();
+		System.out.println(c.getSaldo());
+		System.out.println();
+		
+		while(true) {
+			System.out.println("___________________________________________________________________");
+			System.out.println("                                                                   ");
+			System.out.println("                          PURPLE BANK                              ");
+			System.out.println("                                                                   ");
+			System.out.println("___________________________________________________________________");
+			System.out.println("                                                                   ");
+			System.out.println("                   1- Abrir Conta                                  ");
+			System.out.println("                   2- Listar todas as contas                       ");
+			System.out.println("                   3- Buscar conta por número                      ");
+			System.out.println("                   4- Atualizar dados da conta                     ");
+			System.out.println("                   5- Desativar conta                              ");
+			System.out.println("                   6- Saque                                        ");
+			System.out.println("                   7- Depositar                                    ");
+			System.out.println("                   8- Transferência entre contas                   ");
+			System.out.println("                   9- Sair                                         ");
+			System.out.println("                                                                   ");
+			System.out.println("___________________________________________________________________");
+			System.out.println(">>>> Digite sua opção:                                             ");
+			System.out.println("                                                                   " + Cores.TEXT_RESET);
+			opcao = leia.nextInt();
+			if(opcao == 9) {
+				System.out.println("Sistema finalizado");
+				System.exit(0);
+			}
+			switch(opcao){
+			case 1 ->{
+				System.out.println("Criar conta\n");
+			}
+			case 2 ->{
+				System.out.println("Listar todas as contas\n");
+			}
+			case 3 ->{
+				System.out.println("Consultar dados da conta por número\n");
+			}
+			case 4 ->{
+				System.out.println("Atualizar dados da conta\n");
+			}
+			case 5 ->{
+				System.out.println("pagar conta\n");
+			}
+			case 6 ->{
+				System.out.println("Saque\n");
+			}
+			case 7 ->{
+				System.out.println("Depósito\n");
+			}
+			case 8 ->{
+				System.out.println("Transferência entre contas\n");
+			}
+			default ->System.out.println("Opção inválida!");
+			}
+			
+		}
 
 	}
 
--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -1,5 +1,78 @@
 package conta.model;
 
+import conta.util.Cores;
+
 public class Conta {
+	
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private float saldo;
+	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public float getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	public boolean sacar(float valor) {
+		if(this.getSaldo() < valor) {
+			System.out.println(Cores.TEXT_PURPLE_BOLD+"\nSaldo insuficiente..."+ Cores.TEXT_RESET);
+			return false;
+		}
+		this.setSaldo(this.getSaldo() - valor);
+		System.out.println("Saldo Atual: "+ getSaldo());
+		return true;
+	}
+	public void depositar(float valor) {
+		this.setSaldo(this.getSaldo() + valor);
+	}
+	public void visualizar() {
+		String tipo = "";
+		switch(this.tipo) {
+		case 1 -> tipo = "Conta Corrente";
+		case 2 -> tipo = "Conta Poupança";
+		}
+		System.out.println("_____________________________");
+		System.out.println("     GERENCIADOR DE CONTAS   ");
+		System.out.println("_____________________________");
+		System.out.println("Conta: "+ this.numero);
+		System.out.println("Agência: "+ this.agencia);
+		System.out.println("Tipo de Conta: "+ tipo);
+		System.out.println("Nome do Titular: "+ this.titular);
+		System.out.println("Saldo da Conta: "+ this.saldo + "\n\n");
+	}
+	
 
 }
