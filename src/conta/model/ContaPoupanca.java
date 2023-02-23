0c998d50ff66849ee3e5047ff7eb4f4636df3a22
lialaurindo <liamaralaurindo@gmail.com>
2023-02-22 23:45:05 -0300

    cria subclasse Conta Poupança

--- src/conta/model/ContaPoupanca.java
+++ src/conta/model/ContaPoupanca.java
@@ -1,5 +1,27 @@
 package conta.model;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
 
+	private int aniversarioConta;
+
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversarioConta) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversarioConta = aniversarioConta;
+	}
+
+	public int getAniversarioConta() {
+		return aniversarioConta;
+	}
+
+	public void setAniversarioConta(int aniversarioConta) {
+		this.aniversarioConta = aniversarioConta;
+	}
+
+	 @Override
+		public void visualizar() {
+			super.visualizar();
+			System.out.println("Aniversário da conta: " + this.aniversarioConta);
+		}
+	
+	
 }
