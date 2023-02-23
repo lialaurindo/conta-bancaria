ee72be52128a1beeeb177787bfd5183c2322644a
lialaurindo <liamaralaurindo@gmail.com>
2023-02-23 13:07:14 -0300

    implementa métodos CRUD da Interface ContaController

---
+++ src/conta/controller/ContaController.java
@@ -0,0 +1,48 @@
+package conta.controller;
+
+import conta.model.Conta;
+import conta.repository.ContaRepository;
+
+public class ContaController implements ContaRepository {
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		
+	}
+
+	@Override
+	public void listasTodas() {
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		
+	}
+
+	@Override
+	public void transferir(int numero, int numerodestino, float valor) {
+		
+	}
+
+}
