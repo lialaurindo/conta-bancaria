27a130ee53340ab46741f85371075390196a4868
lialaurindo <liamaralaurindo@gmail.com>
2023-02-24 13:42:47 -0300

    inclui parametros e condições nos metodos + criação de metodos auxiliares

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -1,48 +1,105 @@
 package conta.controller;
 
+import java.util.ArrayList;
+
 import conta.model.Conta;
 import conta.repository.ContaRepository;
 
 public class ContaController implements ContaRepository {
 
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+
 	@Override
 	public void procurarPorNumero(int numero) {
-		
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("A Conta número: " + numero + " não foi encontrada!");
+
 	}
 
 	@Override
-	public void listasTodas() {
-		
+	public void listarTodas() {
+		for (var conta : listaContas)
+			conta.visualizar();
 	}
 
 	@Override
 	public void cadastrar(Conta conta) {
-		
+		listaContas.add(conta);
+		System.out.println("A conta foi criada!");
 	}
 
 	@Override
 	public void atualizar(Conta conta) {
-		
+		var buscaConta = buscarNaCollection(conta.getNumero());
+
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("A Conta número: " + conta.getNumero() + " foi atualizada!");
+		} else
+			System.out.println("A Conta número: " + conta.getNumero() + " não foi encontrada!");
+
 	}
 
 	@Override
 	public void deletar(int numero) {
-		
+		var conta = buscarNaCollection(numero);
+
+		if (conta != null) {
+			if (listaContas.remove(conta) == true)
+				System.out.println("A Conta número: " + numero + " foi deletada!");
+		} else
+			System.out.println("A Conta número: " + numero + " não foi encontrada!");
+
 	}
 
 	@Override
 	public void sacar(int numero, float valor) {
-		
+
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
-		
+
 	}
 
 	@Override
 	public void transferir(int numero, int numerodestino, float valor) {
-		
+
 	}
 
+	/* Implemento de Métodos Auxiliares */
+
+	public int gerarNumero() {
+		return listaContas.size() + 1;
+
+	}
+
+	// Percorre a Collection para buscar se o numero digitado está lá
+	// Caso não ache retorna nulo.
+
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero)
+				return conta;
+		}
+
+		return null;
+	}
+
+	
+	public int retornaTipo(int numero) {
+
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero)
+				return conta.getTipo();
+		}
+
+		return 0;
+		
+	}
+	
 }
